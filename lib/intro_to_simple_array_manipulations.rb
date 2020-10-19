bbba5e45d4fa08e7ca129729543cf583e6af6243
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-10-19 20:28:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,19 @@ end
 def using_insert(a,b)
   a.insert(4, b)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(a,b)
+  a.delete(b)
+end
+
+def using_delete_at(a,b)
+  a.delete_at(b)
+end
